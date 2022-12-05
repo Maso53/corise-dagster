49e4aa01630dbbd7f81a35335b94923f4a61a4a8
Maso53 <118852364+Maso53@users.noreply.github.com>
2022-12-04 21:34:06 -0600

    Not as confident as last week...

--- week_2/workspaces/project/week_2.py
+++ week_2/workspaces/project/week_2.py
@@ -5,29 +5,41 @@ from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
 
-@op
-def get_s3_data():
-    pass
-
-
-@op
-def process_data():
-    pass
-
-
-@op
-def put_redis_data():
-    pass
-
+@op(
+    config_schema={"s3_key": String},
+    required_resource_keys={"s3"},
+)
+def get_s3_data(context):
+    key = context.op_config["s3_key"]
+    s3 = context.resources.s3
+    return [Stock.from_list(x) for x in s3.get_data(key)]
 
 @op
-def put_s3_data():
-    pass
+def process_data(context, stock:list) -> Aggregation:
+    max_item = Aggregation(date=datetime.now(), high=0.0)
+    for stock in stock:
+        if stock.high > max_item.high:
+            max_item = Aggregation(date=stock.date, high=stock.high)
+    return max_item
+
+@op(
+    required_resource_keys={'redis'},
+)
+def put_redis_data(context, aggregation: Aggregation):
+    context.resources.redis.put_data(date(aggregation.date), aggregation.high)
+
+@op(
+    required_resource_keys={"s3"},
+)
+def put_s3_data(context, aggregation: Aggregation):
+    context.resources.s3.put_data(date(aggregation.date), aggregation.high)
 
 
 @graph
 def week_2_pipeline():
-    pass
+    aggregation = process_data(get_s3_data())
+    put_redis_data(aggregation)
+    put_s3_data(aggregation)
 
 
 local = {
@@ -54,10 +66,17 @@ docker = {
     "ops": {"get_s3_data": {"config": {"s3_key": "prefix/stock.csv"}}},
 }
 
-week_2_pipeline_local = week_2_pipeline.to_job(
-    name="week_2_pipeline_local",
+local_week_2_pipeline = week_2_pipeline.to_job(
+    name="local_week_2_pipeline",
+    config=local,
+    resource_defs={"s3": mock_s3_resource, "redis": ResourceDefinition.mock_resource()},
 )
 
 week_2_pipeline_docker = week_2_pipeline.to_job(
     name="week_2_pipeline_docker",
+    config=docker,
+    resource_defs={
+        "s3": s3_resource,
+        "redis": redis_resource
+    }
 )
