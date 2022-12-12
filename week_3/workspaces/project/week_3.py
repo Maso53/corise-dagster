819f1d14c08fd41e2c2f3ae825634a58732a0b80
Maso53 <118852364+Maso53@users.noreply.github.com>
2022-12-11 18:17:58 -0600

    Up to graph

--- week_3/workspaces/project/week_3.py
+++ week_3/workspaces/project/week_3.py
@@ -8,7 +8,9 @@ from dagster import (
     RetryPolicy,
     RunRequest,
     ScheduleDefinition,
+    ScheduleEvaluationContext,
     SkipReason,
+    String,
     graph,
     op,
     schedule,
@@ -20,29 +22,41 @@ from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
 
-@op
-def get_s3_data():
-    pass
+@op(
+    config_schema={"s3_key": String},
+    required_resource_keys={"s3"}
+)
+def get_s3_data(context) -> List[Stock]:
+    s3_key = context.op_config["s3_key"]
+    return [Stock.from_list(stock) for stock in context.resources.s3.get_data(s3_key)]
 
 
 @op
-def process_data():
-    pass
+def process_data(context, stocks: List[Stock]) -> Aggregation:
+    highest_stock = max(stocks, key=lambda stock:stock.high)
+    return Aggregation(date=highest_stock.date, high=highest_stock.high)
 
 
-@op
-def put_redis_data():
-    pass
+@op (
+    required_resource_keys={"redis"}
+)
+def put_redis_data(context, stock_aggregation: Aggregation):
+    context.resources.redis.put_data(name=str(stock_aggregation.date), value=str(stock_aggregation.high))
 
 
-@op
-def put_s3_data():
-    pass
+@op (
+    required_resource_keys={"s3"}
+)
+def put_s3_data(context, stock_aggregation: Aggregation):
+    context.resources.s3.put_data(key=stock_aggregation.date, data=stock_aggregation)
 
 
 @graph
 def week_3_pipeline():
-    pass
+    stocks = get_s3_data()
+    processed = process_data(stocks)
+    put_redis_data(processed)
+    put_s3_data(processed)
 
 
 local = {
